d6ad6e0c4d4267ee4fd58adb3bef29c21a575e66
João Marcelo <joaosantiago558@gmail.com>
2023-12-15 20:50:32 -0300

    Iniciando a criação da pagina de Menu

--- src/main/java/com/site/SitePessoaal/controler.java
+++ src/main/java/com/site/SitePessoaal/controler.java
@@ -2,17 +2,13 @@ package com.site.SitePessoaal;
 
 import com.site.DataBase.DbSQL;
 import com.site.User.User;
-import java.lang.annotation.Target;
-import javax.security.auth.message.callback.PrivateKeyCallback;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 //@RequestMapping("/Cadastro")
 public class controler {

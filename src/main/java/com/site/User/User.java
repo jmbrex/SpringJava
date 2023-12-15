3559c4f83397605f363ef5981eb2f7cc651948fd
João Marcelo <joaosantiago558@gmail.com>
2023-12-15 20:43:22 -0300

    Iniciando a criação da pagina de Menu

--- src/main/java/com/site/User/User.java
+++ src/main/java/com/site/User/User.java
@@ -7,11 +7,11 @@ public class User {
     public User(){}
 
     public User(String name, String email, String senha, String nick, int ID) {
+        this.ID = ID;
         this.name = name;
         this.email = email;
         this.senha = senha;
         this.nick = nick;
-        this.ID = ID;
     }
 
     public String getName() {
